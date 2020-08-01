17395c128bc33c639fd21b167b155412925eda64
jadorex3 <dron9600@mail.ru>
2020-08-01 17:56:46 +0300

    Refactor template Refactor view in CBV

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from movies.models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Детальное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', {'movie': movie})
+    model = Movie
+    slug_field = 'url'
